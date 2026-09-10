16d808b7ba0b5f80d5911e4ad44e15dd9d2d5a8e
m6jm4s654p-lab <m6jm4s654p@privaterelay.appleid.com>
2026-09-10 15:46:17 +0900

    Add files via upload
    v1.6

--- sw.js
+++ sw.js
@@ -1,5 +1,22 @@
-const CACHE='snowtech-v1.5';
+const CACHE='snowtech-v1.6';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached=>cached || fetch(event.request))
+  );
+});
